6bcfc1a1f2b5b8a485d12c0e75a29107f8bb39bf
DUSTI-JOHNSON\dusti <dustijohnson@outlook.com>
2022-08-14 13:12:09 -0600

    week4 assignment

--- assignments_and_practice/w4_assignment_dusti.py
+++ assignments_and_practice/w4_assignment_dusti.py
@@ -10,7 +10,9 @@ while True:
     transaction = float(input("Enter amount to withdraw (-) or deposit (+): "))
 
     amount = round(amount + transaction, 2)
-    if amount:
+    if amount > 0:
         print(f"Your account balance is ${amount:.2f}")
+    elif amount < 0:
+        print(f"Your account is overdrawn. The overdrawn amount is ${amount:.2f}")
     else:
         print("Your account balance is zero")
