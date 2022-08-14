c477e02e39158700f4c4d209739170b063c1d7b2
DUSTI-JOHNSON\dusti <dustijohnson@outlook.com>
2022-08-14 13:31:49 -0600

    week4 assignment

--- assignments_and_practice/w4_assignment_dusti.py
+++ assignments_and_practice/w4_assignment_dusti.py
@@ -1,14 +1,18 @@
 
 while True:
+    # get name from user
     name = input("Enter your name or 'q' to quit: ")
 
+    # if user inputs q then exit loop
     if name == "q":
         print("Exiting Program")
         break
 
+    # get amount and transaction input from user
     amount = float(input("Enter the amount of your account balance: "))
     transaction = float(input("Enter amount to withdraw (-) or deposit (+): "))
 
+    # get the new balance and check if account is overdrawn or zero balance
     amount = round(amount + transaction, 2)
     if amount > 0:
         print(f"Your account balance is ${amount:.2f}")
