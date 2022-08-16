b8e78260c27edf64364a84ea923d929fa35852da
Robert.Timberlake <robert.c.timberlake@gmail.com>
2022-08-16 16:47:17 -0500

    Week 6 of the professor not doing the reading before making the assignment

--- book_readings/w6.9_reading.py
+++ book_readings/w6.9_reading.py
@@ -80,3 +80,97 @@ class Student(object):
         """Returns the string representation of the student."""
         return "Name: " + self.name + "\nScores: " + \
                " ".join(map(str, self.scores))
+# ====================================================================================================================
+# Built-in Arithmetic Operators and Their Corresponding Methods
+"""
+Operator	Method Name
++	          __add__
+−	          __sub__
+*	          __mul__
+/	          __div__
+%	          __mod__
+"""
+# Code for addition operation
+def __add__(self, other):
+    """Returns the sum of the numbers. self is the left operand and
+    other is the right operand"""
+    new_number = self.number * other.denom + / other.denom * selfdenom
+    new_denom = self.denom * other.denom
+    return Rational(new_number, new_denom)
+# ====================================================================================================================
+# The Comparison Operators and Methods
+"""
+Operator	Meaning	                Method
+==	        Equals	                __eq__
+!=	        Not equals	            __ne__
+<	        Less than	            __lt__
+<=	        Less than or equal	    __le__
+>	        Greater than	        __gt__
+>=	        Greater than or equal	__ge__
+"""
+def __it__(self, other):
+    """Compares two rationa numbers, self and other, using <."""
+    extremes = self.numer * other.denom
+    means = other.numer * self.denom
+    return extremes < means
+# ====================================================================================================================
+def __eq__(self, other):
+    """Tests self and other for equality"""
+    if self is other:  # Object identity?
+        return true
+    elif type(self) != type(other):  # types match?
+        return False
+    else:
+        return self.numer == other.numer and \
+            self.denom == other.denom
+# ====================================================================================================================
+"""
+SavingsAccount 
+Method	               What It Does
+ 
+a = SavingsAccount  Returns a new account with the given name, 
+(name,pin, balance  PIN, and balance.
+= 0.0) 
+a.deposit(amount)	Deposits the given amount to the account’s 
+                    balance.
+a.withdraw(amount)	Withdraws the given amount from the account’s   
+                    balance.
+a.getBalance()	    Returns the account’s balance.
+a.getName()	        Returns the account’s name.
+a.getPin()	        Returns the account’s PIN.
+a.computeInterest()	Computes the account’s interest and deposits    
+                    it.
+a.__str__()	        Same as str(a). Returns the string 
+                    representation of the account.   
+"""
+# ====================================================================================================================
+# pickle
+import pickle
+
+def save(self, file_name = None):
+    """Saves pickled accounts to a file. The parameter allows
+    the user to change filenames."""
+    if file_name != None:
+        self.file_name = file_name
+    elif self.file_name == None:
+        return
+    file_obj = open(self. file_name, "wb")
+    for account in self.accounts.values():
+        pickle.dump(account, file_obj)
+    file_obj.close()
+# ====================================================================================================================
+def __init__(self, file_name = None):
+    """Creates a new dictionary to hold the accounts. If a
+    filename is provided, loads the accounts from a file of
+    pickled accounts."""
+    self.accounts = {}
+    self.filename = file_name
+    if file_name != None:
+        file_obj = open(file_name, "rb")
+        while True:
+            try:
+                account = pickle.load(file_obj)
+                self.add(account)
+            except EOFError:
+                file_obj.clos()
+                break
