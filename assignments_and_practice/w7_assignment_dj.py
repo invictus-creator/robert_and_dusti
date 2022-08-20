df11e45c5050554e1b1e31ab609b567b7e9c91ea
DUSTI-JOHNSON\dusti <dustijohnson@outlook.com>
2022-08-20 15:25:51 -0600

    first attempt at the stupid assignment

--- assignments_and_practice/w7_assignment_dj.py
+++ assignments_and_practice/w7_assignment_dj.py
@@ -1,16 +1,35 @@
 import turtle
-frogscreen = turtle.Screen()
-frogscreen.bgcolor("blue")
-frogger = turtle.Turtle()
-frogger.shape("turtle")
-frogger.color("red")
 
-frogger.penup()
+# the coordinates
+# of each corner
+shape = ((0, 0), (10, 25), (20, 0), (10, -7))
+
+# registering the new shape
+turtle.register_shape('diamond', shape)
+
+screen = turtle.Screen()
+screen.bgcolor('blue')
+
+gary = turtle.Turtle()
+gary.shape("diamond")
+gary.color('red')
+
+timer = 0
 size = 20
-for i in range(20):
-    frogger.stamp()             # leave an impression on the canvas
-    size = size + 3             # increase the size on every iteration
-    frogger.forward(size)       # move frogger along
-    frogger.right(24)           # ... and turn her
 
-frogscreen.mainloop()
+
+def start_timer():
+    global timer, gary, size
+    timer += 1
+    gary.stamp()
+    size = size + 1
+    gary.forward(size)
+    gary.right(25)
+    if timer >= 60: # 60 seconds
+        return
+    screen.ontimer(start_timer, 1000) # 1 second
+
+
+start_timer()
+
+screen.mainloop()
